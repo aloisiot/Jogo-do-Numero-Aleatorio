b7825e787d4b2db3a62397abf2c38876f02ccf10
aloisiot <loistc.carvalho@gmail.com>
2021-08-08 13:43:12 -0300

    first commit

---
+++ src/random-number-game/random-number-game-linux.c
@@ -0,0 +1,156 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int tentativas;
+int recorde;
+int rNumber;
+int option;
+int newGame;
+int resp;
+
+int RandomN()
+{
+	int x;
+	srand(time(NULL));
+	x = rand()% 10;
+	return x;
+}
+
+
+ void Menu()
+ {
+ 	int imp;
+	
+	printf("\nEscolha uma das opcoes:\n");
+	printf("1 Novo jogo \n");
+	printf("2 Ver recorde \n");
+	printf("3 Limpar recorde \n");
+	printf("4 Sair \n\n");
+	
+	scanf("%d", &imp);
+	
+	if (imp<= 4 || imp >=1)
+ 	{
+ 		option= imp;	
+	}
+	 else 
+	{
+	 	Menu();
+	}
+ }
+ 
+ int NextAction()
+{
+	int resp = 0;
+
+	printf("\nEscolha uma das opcoes:\n");
+	printf("Novo Jogo: 1 \n");
+	printf("Menu Principal: 0 \n");
+	scanf("%d", &resp);
+	
+	if (resp==1 || resp == 0)
+ 	{
+		return resp; 
+	}
+	 else 
+	{
+	 	NextAction();
+	}
+}
+
+ 
+void Game()
+{
+		
+	int chute;
+	printf("Escolha um numero entre 0 e 10 \n");
+	printf("Insira um numero negativo para sair do jogo \n\n");
+	scanf("%d", &chute);
+	
+	tentativas++;
+	
+		
+	if(chute < 0)
+	{
+		system("clear");
+		printf("\nPartida finalizada. \n\n");
+	}
+	else if(chute == rNumber)
+	{
+		system("clear");
+		printf("\nPARABENS, VOCE ACERTOU!!! \n\n");
+		recorde ++;
+	}
+	else if (chute < rNumber) 
+	{
+		printf("\nTente um numero Maior! \n");
+		Game();
+	}
+	else if (chute > rNumber)
+	{
+		printf("\nTente um numero Menor! \n");
+		Game();
+	}
+	
+}
+ 
+void Action(int opt)
+ {
+ 	
+ 	
+  	if(opt == 1 )
+ 	{
+		system("clear");
+ 		rNumber=RandomN();
+		printf("\nUm numero foi sorteado. \nTente adivinha-lo!\n");
+		Game();
+
+		while (NextAction()==1)
+		{
+			rNumber=RandomN();
+			printf("\nUm numero foi sorteado. \nTente adivinha-lo!\n");
+			Game();
+		}
+			
+		Menu();
+		Action(option);
+ 		
+	}
+	else if (opt == 2)
+	{
+		system("clear");
+		printf("\nO recorde atual e: %d\n", recorde); 
+		Menu();
+		Action(option);
+		
+	}
+	else if(opt == 3)
+	{
+		system("clear");
+		recorde=0;
+		printf("Recorde zerado.");
+		Menu();
+		Action(option);
+	}
+	else if(opt == 4)
+	{
+		system("clear");
+		printf("\nAte a proxima \n");
+	}	
+ 	
+ }
+
+
+int main(){
+	
+	system("clear");
+	printf("\nOla, bem vida(o)! \n");
+	
+	Menu();
+	
+	Action(option);
+	
+	return 0;
+
+}
